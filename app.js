cb2ee12676480e0315e161f11899f86f8da7166a
Brian Hyun <jso.venn@gmail.com>
2020-07-25 00:07:33 -0700

    Move index and error route handling to routes and controller directories respectively

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const indexRouter = require('./routes/index');
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -8,11 +11,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res, next) => {
-	res.render('index', {
-		pageTitle: 'Home Page'
-	});
-});
+// Route Handling
+app.use(indexRouter);
+app.use(errorController); 
 
 app.listen(3000, () => {
 	console.log('Blog App listening on port', 3000);
